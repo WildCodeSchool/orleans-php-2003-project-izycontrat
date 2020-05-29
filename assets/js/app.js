b4416e3983321c07118f5bb28087e290d00780f0
Tobe_Continued <do.you.continue@gmail.com>
2020-05-29 16:13:40 +0200

    Installation de Bootstrap dans le Projet

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,9 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-
+const $ = require('jquery');
 // any CSS you require will output into a single css file (app.css in this case)
+require('bootstrap');
 require('../scss/app.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
