ab693a87c3c1be3ef23c0dae87bd2ad95871d87f
Olivier <olivier.monsire@gmail.com>
2020-06-04 15:02:20 +0200

    fix indentation

--- assets/js/_navbar.js
+++ assets/js/_navbar.js
@@ -1,18 +1,9 @@
-$(document)
-    .ready(function () {
-        $(window)
-            .scroll(function () {
-                if ($(this)
-                    .scrollTop() > 100) {
-                    $('.navbar')
-                        .css('background-color', '#042A5F');
-                    $('.nav-link')
-                        .css('color', '#E1E6EC');
-                } else {
-                    $('.navbar')
-                        .css('background-color', 'rgba(0,0,0,0.0)');
-                    $('.nav-link')
-                        .css('color', '#000A16');
-                }
-            });
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.navbar').css('background-color', '#042A5F');
+        } else {
+            $('.navbar').css('background-color', 'rgba(0,0,0,0.0)');
+        }
     });
+});
