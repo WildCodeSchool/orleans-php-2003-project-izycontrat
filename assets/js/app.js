507d552d4f0c7ca4a76f3ecfd3233280e832c5ed
Tobe_Continued <do.you.continue@gmail.com>
2020-06-04 15:14:44 +0200

    fix Jquery Call

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-global.$ = global.jQuery = require('jquery');
+global.$ = require('jquery');
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('bootstrap');
