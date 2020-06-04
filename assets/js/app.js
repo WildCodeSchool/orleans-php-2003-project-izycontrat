1c7b405cf7f0e19bcaf105619caff81997307d52
Tobe_Continued <do.you.continue@gmail.com>
2020-06-04 15:52:53 +0200

    fix Jquery call

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,6 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-global.$ = require('jquery');
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('bootstrap');
@@ -15,4 +14,4 @@ require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/js/all.js');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+const $ = require('jquery');
