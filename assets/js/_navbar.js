7954039a199cf2a7008bcbfdf53e51ac6eef9b7f
Olivier <olivier.monsire@gmail.com>
2020-06-04 18:02:21 +0200

    fix the navbar scrolling animation, work only on Home now

--- assets/js/_navbar.js
+++ assets/js/_navbar.js
@@ -3,7 +3,7 @@ const $ = require('jquery');
 $(document).ready(() => {
     // eslint-disable-next-line func-names
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
+        if ($(this).scrollTop() > 200) {
             $('.navbar').css('background-color', '#042A5F');
         } else {
             $('.navbar').css('background-color', 'transparent');
--- assets/js/app.js
+++ assets/js/app.js
@@ -5,8 +5,6 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 // any CSS you require will output into a single css file (app.css in this case)
-import './_navbar';
-
 require('bootstrap');
 require('../scss/app.scss');
 require('../scss/home.scss');
