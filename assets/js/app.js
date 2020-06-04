17dc68a4df9836558842742e5c42c39885b57e5f
Tobe_Continued <do.you.continue@gmail.com>
2020-06-04 16:14:06 +0200

    fix Jquery call

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,4 +14,4 @@ require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/js/all.js');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-global.$ = require('jquery');
+const $ = require('jquery');
