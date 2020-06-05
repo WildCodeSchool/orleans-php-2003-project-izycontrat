86fa28357e28db94954516303ce03fd70d594cbf
Tobe_Continued <do.you.continue@gmail.com>
2020-06-05 16:28:45 +0200

    Modification de la charte graphique du site

--- assets/js/_navbar.js
+++ assets/js/_navbar.js
@@ -4,7 +4,7 @@ $(document).ready(() => {
     // eslint-disable-next-line func-names
     $(window).scroll(function () {
         if ($(this).scrollTop() > 200) {
-            $('.navbar').css('background-color', '#042A5F');
+            $('.navbar').css('background-color', '#2d2d2d');
         } else {
             $('.navbar').css('background-color', 'transparent');
         }
