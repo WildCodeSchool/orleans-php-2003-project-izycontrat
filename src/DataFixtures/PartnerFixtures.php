1423ea9b7c44815710f17574e32de1057acbea76
Grefleon <oleg.chenesseau@gmail.com>
2020-06-10 15:31:31 +0200

    Setting up partners fixtures

--- src/DataFixtures/PartnerFixtures.php
+++ src/DataFixtures/PartnerFixtures.php
@@ -2,18 +2,26 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Partner;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class PartnerFixtures extends Fixture
 {
-    const NB_PARTNERS = 8;
+    const NB_PARTNERS = 6;
 
     public function load(ObjectManager $manager)
     {
-        
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create();
+
+        for ($i = 0; $i < self::NB_PARTNERS; $i++) {
+            $partner = new Partner();
+            $partner->setName($faker->sentence(rand(1, 5)));
+            $partner->setLink('https://www.google.com');
+            $partner->setPicture("https://via.placeholder.com/" . rand(200, 500) . 'x' . rand(200, 500));
+            $manager->persist($partner);
+        }
 
         $manager->flush();
     }
