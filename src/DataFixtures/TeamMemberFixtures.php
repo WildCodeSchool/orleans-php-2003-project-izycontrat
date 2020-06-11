36126b9b218472148e95b77185b4a6961adeba64
Oleg Chenesseau <oleg.chenesseau@gmail.com>
2020-06-11 16:17:28 +0200

    Setting up members fixtures

--- src/DataFixtures/TeamMemberFixtures.php
+++ src/DataFixtures/TeamMemberFixtures.php
@@ -2,15 +2,28 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\TeamMember;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class TeamMemberFixtures extends Fixture
 {
+    const NB_MEMBERS = 6;
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < self::NB_MEMBERS; $i++) {
+            $teamMember = new TeamMember();
+            $teamMember->setName($faker->name());
+            $teamMember->setRole(ucfirst(implode(' ', $faker->words())));
+            $teamMember->setDescription($faker->realText());
+            $teamMember->setPicture('https://via.placeholder.com/500x1000');
+
+            $manager->persist($teamMember);
+        }
 
         $manager->flush();
     }
