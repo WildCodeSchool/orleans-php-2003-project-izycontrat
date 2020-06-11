b4c82dfa4258727abb3e55cd1a2532f5912e65b3
Tobe_Continued <do.you.continue@gmail.com>
2020-06-11 15:25:08 +0200

    Amélioration du Dashboard, correction du collapse en cas de click sur les dropdown

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Security\UserAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
