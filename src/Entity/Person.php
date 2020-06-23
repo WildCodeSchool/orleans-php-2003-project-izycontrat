67d0729d822df08b3f4d071bbf7375876c08ce60
Tobe Continued <do.you.continue@gmail.com>
2020-06-23 09:52:22 +0200

    Securisation de l'entité Person

--- src/Entity/Person.php
+++ src/Entity/Person.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\PersonRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=PersonRepository::class)
@@ -19,52 +20,105 @@ class Person
 
     /**
      * @ORM\OneToOne(targetEntity=User::class, cascade={"persist", "remove"})
-     * @ORM\JoinColumn(nullable=false)
+     * @ORM\JoinColumn(nullable=true)
      */
     private $user;
 
     /**
      * @ORM\Column(type="integer", nullable=true)
+     * @Assert\Type(["int", "null"], message="Mauvais format de données")
+     * @Assert\Range(
+     *      min = 0,
+     *      max = 2,
+     *      notInRangeMessage = "Le Genre n'est pas valide",
+     * )
      */
     private $gender;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=50)
+     * @Assert\Type("string", message="Mauvais format de données")
+     * @Assert\Length(
+     *      min = 5,
+     *      max = 50,
+     *      minMessage = "Le Prénom doit contenir d'au moins {{ limit }} characters",
+     *      maxMessage = "Le Prénom doit contenir au maximum {{ limit }} characters",
+     *      allowEmptyString = false
+     * )
      */
     private $firstName;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @ORM\Column(type="string", length=50, nullable=true)
+     * @Assert\Type(["string", "null"], message="Mauvais format de données")
+     * @Assert\Length(
+     *      max = 50,
+     *      maxMessage = "Le deuxième Prénom doit contenir au maximum {{ limit }} characters",
+     *      allowEmptyString = true
+     * )
      */
     private $middleName = null;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=50)
+     * @Assert\Type("string", message="Mauvais format de données")
+     * @Assert\Length(
+     *      min = 5,
+     *      max = 50,
+     *      minMessage = "Le Nom doit contenir d'au moins {{ limit }} characters",
+     *      maxMessage = "Le Nom doit contenir au maximum {{ limit }} characters",
+     *      allowEmptyString = false
+     * )
      */
     private $lastName;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Type("string", message="Mauvais format de données")
+     * @Assert\NotBlank(message="Le numéro de téléphone ne doit pas être vide")
+     * @Assert\Regex(
+     *     pattern="/^(\+33)?(0)?[0-9]{9}$/",
+     *     match=false,
+     *     message="Le numéro de téléphone doit être dans un format valide (+33XXXXXXXXX ou 0XXXXXXXXX)"
+     * )
      */
     private $phoneNumber;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Type("string", message="Mauvais format de données")
+     * @Assert\Length(
+     *      min = 5,
+     *      max = 255,
+     *      minMessage = "L'adresse doit contenir d'au moins {{ limit }} characters",
+     *      maxMessage = "L'adresse doit contenir au maximum {{ limit }} characters",
+     *      allowEmptyString = false
+     * )
      */
     private $address;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @ORM\Column(type="string", length=100, nullable=true)
+     * @Assert\Type(["string", "null"], message="Mauvais format de données")
+     * @Assert\Country(message="Ce n'est pas un pays valide")
+     * @Assert\Length(
+     *      max = 255,
+     *      maxMessage = "Le pays doit contenir au maximum {{ limit }} characters",
+     *      allowEmptyString = true
+     * )
      */
     private $country;
 
     /**
      * @ORM\Column(type="integer", nullable=true)
+     * @Assert\Type(["int", "null"], message="Mauvais format de données")
+     * @Assert\Positive(message="Le Capital apporté ne peux pas être négatif")
      */
     private $capitalAmountAdding;
 
     /**
      * @ORM\Column(type="boolean", nullable=true)
+     * @Assert\Type(["bool", "null"], message="Mauvais format de données")
      */
     private $hasCompany;
 
