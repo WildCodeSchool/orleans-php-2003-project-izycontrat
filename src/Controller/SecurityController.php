484c261a2023e402abbc19cf54bac135cdf82484
Tobe-Continued <do.you.continue@gmail.com>
2020-07-03 12:47:59 +0200

    correction divers bugs

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -17,9 +17,9 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
