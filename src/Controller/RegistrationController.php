3bd486e7b0cb95ecb40c60d93cd7af3237bae11d
Grefleon <oleg.chenesseau@gmail.com>
2020-07-06 10:40:41 +0200

    Setting up lawyer registration method

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -23,7 +23,7 @@ class RegistrationController extends AbstractController
      * @param UserAuthenticator $authenticator
      * @return Response
      */
-    public function register(
+    public function registerUser(
         Request $request,
         UserPasswordEncoderInterface $passwordEncoder,
         GuardAuthenticatorHandler $guardHandler,
@@ -67,7 +67,64 @@ class RegistrationController extends AbstractController
             );
         }
 
-        return $this->render('registration/register.html.twig', [
+        return $this->render('registration/user.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/register/lawyer", name="lawyer_register")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param GuardAuthenticatorHandler $guardHandler
+     * @param UserAuthenticator $authenticator
+     * @return Response
+     */
+    public function registerLawyer(
+        Request $request,
+        UserPasswordEncoderInterface $passwordEncoder,
+        GuardAuthenticatorHandler $guardHandler,
+        UserAuthenticator $authenticator
+    ): ?Response {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
+        $person = new Person();
+        $user = new User();
+        $person->setUser($user);
+        $form = $this->createForm(RegistrationFormType::class, $person);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $person->setAddress($form->get('address')->getData());
+            $person->setCountry($form->get('country')->getData());
+            $person->setFirstName($form->get('firstName')->getData());
+            $person->setLastName($form->get('lastName')->getData());
+            $person->setPhoneNumber($form->get('phoneNumber')->getData());
+            $user->setEmail($request->request->get('registration_form')['user']['email']);
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $request->request->all()['registration_form']['user']['password']
+                )
+            );
+            $user->setIsVerified(false);
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $entityManager->persist($user);
+            $entityManager->persist($person);
+
+            $entityManager->flush();
+
+            return $guardHandler->authenticateUserAndHandleSuccess(
+                $user,
+                $request,
+                $authenticator,
+                'main'
+            );
+        }
+
+        return $this->render('registration/lawyer.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
     }
