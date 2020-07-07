59c97d0b4bf06beb040a84e862f18f8a92154bd5
Olivier <olivier.monsire@gmail.com>
2020-07-07 15:12:18 +0200

    update fixtures

--- src/DataFixtures/PersonFixtures.php
+++ src/DataFixtures/PersonFixtures.php
@@ -10,9 +10,46 @@ use Doctrine\Persistence\ObjectManager;
 use App\Entity\Person;
 use Faker;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
-class PersonFixtures extends Fixture
+class PersonFixtures extends Fixture implements DependentFixtureInterface
 {
+    public function getDependencies()
+    {
+        return [UserFixtures::class];
+    }
+
+    const PEOPLE = [
+        'Jane Doe' => [
+            'infos' => [
+                'Jane',
+                'Doe',
+                1,
+                '09 10 06 07 08',
+                '8 rue Louis Dupas'
+            ]
+        ],
+        'Janine Doe' => [
+            'infos' => [
+                'Jane',
+                'Doe',
+                1,
+                '60 70 80 90 10',
+                '8 avenue Louis Dupat'
+            ]
+        ],
+        'John Doe' => [
+            'infos' => [
+                'John',
+                'Doe',
+                0,
+                '10 09 08 07 06',
+                '8 impasse Luis Dupas'
+            ]
+        ]
+    ];
+
+
     const MEMBER = 20;
     const CLIENT = 20;
 
@@ -25,18 +62,32 @@ class PersonFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        for ($i = 0; $i< self::MEMBER + self::CLIENT; $i++) {
+        $refNumber = 0;
+        foreach (self::PEOPLE as $infos) {
+            $person = new Person();
+            $person->setFirstName(($infos['infos'][0]));
+            $person->setLastName(($infos['infos'][1]));
+            $person->setGender(($infos['infos'][2]));
+            $person->setPhoneNumber(($infos['infos'][3]));
+            $person->setAddress(($infos['infos'][4]));
+            $person->setUser($this->getReference('person_' . $refNumber));
+            $manager->persist($person);
+            $refNumber++;
+        }
+
+
+        for ($i = 0; $i < self::MEMBER + self::CLIENT; $i++) {
             $person = new Person();
             $user = new User();
             $faker = Faker\Factory::create('fr_FR');
             $fakePersons = [
-                ['gender' => null, 'picture' => 'https://statics.lesinrocks.com/content/thumbs/uploads/2018/07/'.
+                ['gender' => null, 'picture' => 'https://statics.lesinrocks.com/content/thumbs/uploads/2018/07/' .
                     'width-1125-height-612-quality-10/capture-decran-2018-07-06-a-13-03-57.png'],
-                ['gender' => 'male', 'picture' => 'https://gal.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.'.
-                '2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Fgal.2Fvar.2Fgal.2Fstorage.2Fimages.2Fmedia.2Fmultiupl'.
-                'oad_du_10_juillet_2017.2Fcoupedecheveux.2F4118549-1-fre-FR.2Fcoupedecheveux.2Ejpg/480x480/quality'.
-                '/80/coiffures-homme-les-coupes-de-cheveux-tendances-de-l-ete-2017.jpg'],
-                ['gender' => 'female', 'picture' => 'https://www.top-metiers.fr/wp-content/uploads/2016/01/'.
+                ['gender' => 'male', 'picture' => 'https://gal.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.' .
+                    '2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Fgal.2Fvar.2Fgal.2Fstorage.2Fimages.2Fmedia.2Fmultiupl' .
+                    'oad_du_10_juillet_2017.2Fcoupedecheveux.2F4118549-1-fre-FR.2Fcoupedecheveux.2Ejpg/480x480/quality'.
+                    '/80/coiffures-homme-les-coupes-de-cheveux-tendances-de-l-ete-2017.jpg'],
+                ['gender' => 'female', 'picture' => 'https://www.top-metiers.fr/wp-content/uploads/2016/01/' .
                     'liste_metier-de-femme.jpg']
             ];
             $fakePerson = $faker->randomElement($fakePersons);
