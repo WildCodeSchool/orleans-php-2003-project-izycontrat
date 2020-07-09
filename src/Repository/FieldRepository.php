ed45a9b01f1f99ed7ca03aeafcd379237e1dd9b0
AnthonyBerduck <34470854+AnthonyBerduck@users.noreply.github.com>
2020-07-09 17:31:42 +0200

    Update FieldRepository.php

--- src/Repository/FieldRepository.php
+++ src/Repository/FieldRepository.php
@@ -18,33 +18,4 @@ class FieldRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Field::class);
     }
-
-    // /**
-    //  * @return Field[] Returns an array of Field objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('f')
-            ->andWhere('f.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('f.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Field
-    {
-        return $this->createQueryBuilder('f')
-            ->andWhere('f.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
